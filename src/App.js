d07ce0ba265934baaf56fde25f0443511a6df706
VatsalBhuva11 <vatsalbhuva11@gmail.com>
2024-01-02 23:59:10 +0530

    1) email-password sign-up (primary) 2) link google account to email (to allow future google sign in) 3) email-password sign-in 4) setup firebase config, auth done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import SignIn from "./SignIn";
+
 export default function App() {
     return (
         <div>
             <h1>Hello, World!</h1>
+            <SignIn />
         </div>
     );
 }
