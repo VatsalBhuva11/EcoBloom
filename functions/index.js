0ec5c5bf8aa00b41e1d92e3fc9040e635a32a761
VatsalBhuva11 <vatsalbhuva11@gmail.com>
2024-01-02 13:02:05 +0530

    1) move schemas inside functions 2) setup mongoose, database, and connection

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,18 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const connectDB = require("./config/db.config.js");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+connectDB();
 
 // https://us-central1-ecobloom-gdsc-challenge.cloudfunctions.net/api
 app.get("/", (req, res) => {
