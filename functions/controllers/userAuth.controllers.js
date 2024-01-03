fe0964f10d948a5c6fa30d1eef923fc1c543c5c9
VatsalBhuva11 <vatsalbhuva11@gmail.com>
2024-01-03 17:09:51 +0530

    remove bcrypt

--- functions/controllers/userAuth.controllers.js
+++ functions/controllers/userAuth.controllers.js
@@ -5,10 +5,11 @@ const router = express.Router();
 // Create a new user
 router.post("/register", async (req, res) => {
     try {
+        const { name, email, password } = req.body;
         const user = await User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
+            name,
+            email,
+            password,
         });
         console.log("Successfully created new user in DB!");
         res.json(user);
