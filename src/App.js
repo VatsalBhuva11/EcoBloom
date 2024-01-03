849102a9588b55aedfcea15d85b9d8aec1e12c18
VatsalBhuva11 <vatsalbhuva11@gmail.com>
2024-01-03 10:05:20 +0530

    .env functional

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import SignIn from "./SignIn";
 
 export default function App() {
-    return (
-        <div>
-            <h1>Hello, World!</h1>
-            <SignIn />
-        </div>
-    );
+    return <SignIn />;
 }
--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import dotenv from "dotenv";
-dotenv.config();
 
+//access env variables by creating .env file in /, and adding REACT_APP_ prefix to each variable
+//run npm start after changing/creating .env file to see changes. after that run npm run build to see changes in production
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.DEPLOYED_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: `${process.env.PROJECT_ID}.appspot.com`,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID,
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_DEPLOYED_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: `${process.env.REACT_APP_PROJECT_ID}.appspot.com`,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
