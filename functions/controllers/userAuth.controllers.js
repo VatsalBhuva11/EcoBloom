3ae90eb0269e75382c532a49700033c4145f5d30
VatsalBhuva11 <vatsalbhuva11@gmail.com>
2024-01-04 22:47:55 +0530

    file upload working (db saving left), busboy instead of multer

--- functions/controllers/userAuth.controllers.js
+++ functions/controllers/userAuth.controllers.js
@@ -1,21 +1,30 @@
 import express from "express";
 import User from "../models/user.model.js";
+import filesUpload from "../middlewares/upload.middleware.js";
+
 const router = express.Router();
 
 // Create a new user
-router.post("/register", async (req, res) => {
+router.post("/register", filesUpload, async (req, res) => {
     try {
         const { name, email, password } = req.body;
-        const user = await User.create({
-            name,
-            email,
-            password,
-        });
-        console.log("Successfully created new user in DB!");
-        res.json(user);
+        const profile = req.files[0];
+        console.log(req.files[0]);
+        console.log(name, email, password, profile.originalname);
     } catch (err) {
-        res.json({ message: err });
+        console.log(err);
+        res.send(err);
     }
 });
 
 export default router;
+
+// const { name, email, password } = req.body;
+
+// const user = await User.create({
+//     name,
+//     email,
+//     password,
+// });
+// console.log("Successfully created new user in DB!");
+// res.json(user);
