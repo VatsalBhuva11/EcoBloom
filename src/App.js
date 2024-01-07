4db4eeb763e292fec25bd188f20fe414ff3644df
Rishi Upadhyay <rupadhyay2674@gmail.com>
2024-01-07 18:47:19 +0530

    hero component added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 
 import About from "./Components/About";
+import Hero from "./Components/Hero";
 import Navbar from "./Components/Navbar";
 import SignIn from "./SignIn";
 
 export default function App() {
     return (
         <div>
-            <Navbar />;
-          
-            <About/>
+            <Navbar />
+            <Hero/>           
+            <About />
         </div>
     );
 }
