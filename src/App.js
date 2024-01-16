00d279e0108976e57a738b6576f84ccc10dfed79
nijval21 <nijvalchaudhari043@gmail.com>
2024-01-16 18:01:25 +0530

    User Dashboard

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 // import OrganizationAuth from "./OrganizationAuth";
 import Home from "./Pages/Home";
-// import UserDashboard from './Pages/UserDashboard';
+import UserDashboard from './Pages/UserDashboard';
 // import SignupInitial from './Pages/SignupInitial';
 
 export default function App() {
     return (
         <div>
             {/* <SignupInitial/> */}
-            <Home />
-            {/* <UserDashboard/> */}
+            {/* <Home /> */}
+            <UserDashboard/>
             {/* <SignIn></SignIn> */}
             {/* <OrganizationAuth></OrganizationAuth> */}
         </div>
--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -174,7 +174,7 @@ export default function Signup() {
                                             type="file"
                                             name="profile"
                                             id="profile"
-                                            class="  text-gray-900 sm:text-sm block w-full p-2.5 placeholder-gray-700 placeholder-opacity-70"
+                                            class=" bg-[#0F1035] text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center hover:scale-105 duration-300"
                                             accept="image/*"
                                             required
                                         />
