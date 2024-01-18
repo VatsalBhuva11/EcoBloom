38cec52388495ef4f7feb7bd45cd32347814dae8
VatsalBhuva11 <vatsalbhuva11@gmail.com>
2024-01-19 03:40:08 +0530

    change routes structure, modular, campaign routes

--- functions/routes/organization.routes.js
+++ functions/routes/organization.routes.js
@@ -1,8 +1,17 @@
 import express from "express";
-import organizationRouter from "../controllers/organization.controllers.js";
+import {
+    getOrgDetails,
+    updateOrgPics,
+} from "../controllers/organization.controllers.js";
+import filesUpload from "../middlewares/upload.middleware.js";
+// import checkOrg from "../middlewares/checkOrg.middleware.js";
 
 const router = express.Router();
 
-router.use("/", organizationRouter);
+//
+router.get("/:orgName", getOrgDetails);
+
+// router.patch("/:orgName", checkOrg, filesUpload, updateOrgPics);
+router.patch("/:orgName", filesUpload, updateOrgPics);
 
 export default router;
