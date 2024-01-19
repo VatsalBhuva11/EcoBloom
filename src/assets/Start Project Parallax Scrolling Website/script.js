b09210285378ce426d894a51fc55f8559d8103f4
VatsalBhuva11 <vatsalbhuva11@gmail.com>
2024-01-20 04:01:23 +0530

    home page animation and navbar update

---
+++ src/assets/Start Project Parallax Scrolling Website/script.js
@@ -0,0 +1,5 @@
+let text = document.getElementById('text');
+let leaf = document.getElementById('leaf');
+let hill1 = document.getElementById('hill1');
+let hill4 = document.getElementById('hill4');
+let hill5 = document.getElementById('hill5');
