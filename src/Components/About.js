02f6aa07eae6649fd21b8f2ff6c7751e28c2c6b0
rishika-g31 <rishikagupta312004@gmail.com>
2024-01-31 15:56:11 +0530

    modified about  page

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,43 +3,34 @@ import about from "../assets/images/about.png";
 import { FaArrowRight } from "react-icons/fa";
 
 export default function About() {
-    return (
-        <div
-            id="about"
-            className="w-full h-screen bg-[url('./assets/images/underground.jpg')] bg-cover justify-between pt-12"
-        >
-            <div className="max-w-[1240px] mx-auto justify-center items-center">
-                <h1 className="py-3 text-[#EEF0E5] text-4xl md:text-6xl text-center font-bold mt-12">
-                    ABOUT US
-                </h1>
-                <div className="grid md:grid-cols-2 px-2 py-12">
-                    <div>
-                        <p className="text-xl md:text-2xl  md:mt-12 text-[#f5d2a8] font-medium text-left">
-                            Step into the world of EcoBloom, where we're not
-                            just a platform. It doesn't matter if you're a
-                            grassroots initiative or an established entity;
-                            EcoBloom is your canvas to amplify your
-                            environmental impact. Join us in this collaborative
-                            effort, where every small action contributes to a
-                            cleaner planet.
-                        </p>
-                        <button className="flex mt-8 py-2 px-6 text-center md:text-3xl text-white border bg-[#0F1035] hover:font-bold hover:bg-transparent hover:border-[#0F1035] hover:text-[#0F1035] rounded-md">
-                            Read More{" "}
-                            <FaArrowRight
-                                className="hidden md:flex ml-4 mt-2 "
-                                size={25}
-                            />
-                        </button>
-                    </div>
-                    <div>
-                        <img
-                            src={about}
-                            className="hidden md:block ml-32 xl:h-[20rem]  h-[15rem]"
-                            alt="ecobloom"
-                        />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div
+      id="about"
+      className="w-full h-screen bg-[url('./assets/images/underground.jpg')] bg-cover justify-between pt-12"
+    >
+      <div className="max-w-[1240px] mx-auto justify-center items-center">
+        <h1 className="py-3 text-[#EEF0E5] text-4xl md:text-6xl text-center font-bold mt-12">
+          ABOUT US
+        </h1>
+        <div className="grid md:grid-cols-2 px-2 py-12">
+          <div>
+            <p className="text-xl md:text-3xl tracking-wide  md:mt-12 text-[#f5d2a8] font-semibold text-left">
+              Welcome to EcoBloom, a thriving ecosystem where environmental
+              enthusiasts, whether they're grassroots initiatives or established
+              entities, come together to make a tangible difference in the
+              world. At EcoBloom, we're not just a platform; we're a vibrant
+              community dedicated to amplifying your environmental impact.
+            </p>
+          </div>
+          <div>
+            <img
+              src={about}
+              className="hidden md:block ml-32 xl:h-[20rem]  h-[15rem]"
+              alt="ecobloom"
+            />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
