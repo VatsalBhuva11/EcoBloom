3abe680172473b18b14dcff3c79da68d0ad60291
Rishi Upadhyay <rupadhyay2674@gmail.com>
2024-05-04 16:22:44 +0530

    animation changes

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,23 +1,25 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import emilychen from "../assets/images/emilychen.jpeg";
-import james from "../assets/images/james.jpg";
-import maria from "../assets/images/maria.jpeg";
-import raj from "../assets/images/raj.jpeg";
+import emilychen from '../assets/images/emilychen.jpeg'
+import james from '../assets/images/james.jpg'
+import maria from '../assets/images/maria.jpeg'
+import raj from '../assets/images/raj.jpeg'
 import { useEffect } from "react";
 import Aos from "aos";
-import "aos/dist/aos.css";
+import "aos/dist/aos.css"
+
+
 
 export default function Carousel() {
-    //     const ref = useRef(null);
-    //   const { scrollYProgress } = useScroll({ target: ref });
-    //   const y = useParallax(scrollYProgress, 300);
+//     const ref = useRef(null);
+//   const { scrollYProgress } = useScroll({ target: ref });
+//   const y = useParallax(scrollYProgress, 300);
 
     const settings = {
         dots: true,
         infinite: true,
-        slidesToShow: 3,
+         slidesToShow: 3,
         slidesToScroll: 1,
         arrows: false,
         autoplay: true,
@@ -46,10 +48,10 @@ export default function Carousel() {
         ],
     };
 
-    useEffect(() => {
+    useEffect(()=>{
         Aos.init();
         Aos.refresh();
-    }, []);
+    },[])
 
     return (
         //responsiveness
@@ -58,18 +60,16 @@ export default function Carousel() {
         //change card carousel
         <div className="w-full relative box-border bg-[#fbfbfa] pt-20 pb-20">
             <div className="flex flex-col justify-center items-center">
-                <h1
-                    data-aos="fade-up"
-                    data-aos-duration="1000"
-                    className=" text-[#0F1035] text-4xl md:text-6xl text-center font-bold "
-                >
+                <h1 
+                data-aos = "fade-up"
+                data-aos-duration = "1000"
+                className=" text-[#277868] text-4xl md:text-6xl text-center font-bold ">
                     TESTIMONIALS
                 </h1>
-                <p
-                    data-aos="fade-up"
-                    data-aos-duration="1000"
-                    className="text-[#0F1035] text-xl md:text-2xl text-center font-bold"
-                >
+                <p 
+                data-aos = "fade-up"
+                data-aos-duration = "1000"
+                className="text-[#277868] text-xl md:text-2xl text-center font-bold">
                     Others have done it so you can too.
                 </p>
             </div>
@@ -77,11 +77,10 @@ export default function Carousel() {
                 <div className="w-full">
                     <Slider {...settings} className="flex justify-center p-16">
                         {data.map((data) => (
-                            <div
-                                data-aos="zoom-in"
-                                data-aos-duration="1000"
-                                className="bg-transparent h-[70%] border-solid border-2 border-black hover:shadow-xl  cursor-pointer flex flex-col justify-center items-center text-black rounded-xl gap-4"
-                            >
+                            <div 
+                            data-aos = "zoom-in"
+                            data-aos-duration = "1000"
+                            className="bg-transparent h-[70%] shadow-lg  border-2 border-[#277868] hover:shadow-xl  cursor-pointer flex flex-col justify-center items-center rounded-xl gap-4">
                                 <div className="h-28 w-full rounded-t-xl justify-center text-center flex items-center">
                                     <img
                                         src={data.img}
@@ -89,15 +88,15 @@ export default function Carousel() {
                                         className="h-20 w-20 rounded-full basis-[25%] md:ml-8 sm:ml-4"
                                     />
                                     <div className="flex flex-col justify-center items-center basis-[75%]">
-                                        <p className="text-xl font-semibold">
+                                        <p className="text-xl font-semibold text-[#277868]">
                                             {data.name}
                                         </p>
-                                        <p className="text-md">
+                                        <p className="text-md text-black">
                                             {data.position}
                                         </p>
                                     </div>
                                 </div>
-                                <div className=" flex flex-col justify-center items-center gap-4 p-4">
+                                <div className=" flex flex-col justify-center items-center gap-4 p-4 text-black">
                                     <p>{data.content}</p>
                                     {/* <button className=" bg-[#304D30] text-white text-lg px-6  py-1 rounded-xl">
                                         Read More!
@@ -121,33 +120,34 @@ const data = [
     //     "As an environmental activist, EcoBloom has revolutionized the way we organize and participate in campaigns. The platform's user-friendly interface and real-time progress tracking make it a game-changer. With EcoBloom, our collective efforts translate into visible impact.",
     //     position: "Environmental Activist",
 
+
     // },
     {
         name: "Emily Chen",
         img: emilychen,
         content:
-            "As a small business owner committed to sustainability, EcoBloom has allowed me to engage my local community in eco-friendly initiative which improves sustainability.",
+       "As a small business owner committed to sustainability, EcoBloom has allowed me to engage my local community in eco-friendly initiative which improves sustainability.",
         position: "Student and Eco-Enthusiast",
     },
     {
         name: "James Anderson",
         img: james,
         content:
-            "As a small business owner committed to sustainability, EcoBloom has allowed me to engage my local community in eco-friendly initiative which improves sustainability.",
+        "As a small business owner committed to sustainability, EcoBloom has allowed me to engage my local community in eco-friendly initiative which improves sustainability.",
         position: "Tech Entrepreneur",
     },
     {
         name: "Dr. Maria Garcia",
         img: maria,
         content:
-            "As a small business owner committed to sustainability, EcoBloom has allowed me to engage my local community in eco-friendly initiative which improves sustainability.",
+        "As a small business owner committed to sustainability, EcoBloom has allowed me to engage my local community in eco-friendly initiative which improves sustainability.",
         position: "Environmental Scientist",
     },
     {
         name: "Raj Patel",
         img: raj,
         content:
-            "As a small business owner committed to sustainability, EcoBloom has allowed me to engage my local community in eco-friendly initiative which improves sustainability.",
+        "As a small business owner committed to sustainability, EcoBloom has allowed me to engage my local community in eco-friendly initiative which improves sustainability.",
         position: "Small Business Owner",
     },
 ];
--- src/Pages/SignupInitial.js
+++ src/Pages/SignupInitial.js
@@ -5,18 +5,18 @@ import { Link } from "react-router-dom";
 export default function SignupInitial() {
     return (
         <div className="bg-[#fbfbfa] flex flex-col justify-center items-center p-11 h-screen">
-            <div className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-[#0f1035] font-bold font-inter mb-10 ">
+            <div className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-[#277868] font-bold font-inter mb-10 ">
                 JOIN US AS AN
             </div>
             <img src={banner} alt="" className="hidden sm:flex w-[49%]" />
             <div className="flex flex-col md:gap-20 items-center sm:flex-row sm:items-center md:text-xl ">
                 <Link to="/signup/user">
-                    <button className="bg-[#0f1035] text-white p-4 m-4 rounded-lg font-bold hover:scale-105 duration-300">
+                    <button className="bg-[#277868] text-[#dbdbdb] p-4 m-4 rounded-lg font-bold hover:scale-105 duration-300">
                         Individual
                     </button>
                 </Link>
                 <Link to="/signup/org">
-                    <button className="bg-[#0f1035] text-white p-4 m-4 rounded-lg font-bold hover:scale-105 duration-300">
+                    <button className="bg-[#277868] text-[#dbdbdb] p-4 m-4 rounded-lg font-bold hover:scale-105 duration-300">
                         Organisation
                     </button>
                 </Link>
